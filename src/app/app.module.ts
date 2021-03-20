bb1656b51e88605e95d0c780dbc4eb9db469ac10
Ilja Boitsuk <ilboit@ttu.ee>
2021-03-20 14:49:58 +0200

    #4 Divide form and table on components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
+import { FeedbackTableComponent } from './feedback-table/feedback-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackFormComponent,
+    FeedbackTableComponent
   ],
   imports: [
     BrowserModule,
