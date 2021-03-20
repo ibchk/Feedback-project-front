a9ed568b181bfa108b61135d388b0119206bd3b0
Ilja Boitsuk <ilboit@ttu.ee>
2021-03-20 20:44:34 +0200

    #6 all gaps now checks validation

--- src/app/feedback-form/feedback-form.component.ts
+++ src/app/feedback-form/feedback-form.component.ts
@@ -1,33 +1,84 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormBuilder, FormControl, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import {FormGroup, FormBuilder, FormControl, Validators, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-feedback-form',
   templateUrl: './feedback-form.component.html',
   styleUrls: ['./feedback-form.component.scss']
 })
-export class FeedbackFormComponent implements OnInit {
+export class FeedbackFormComponent {
 
   feedbackForm: FormGroup;
   // nameControl;
   // make subcategories for easier understanding
-  healthC: any = ['-- Patients portal', '-- Doctors portal'];
-  remoteC: any = ['--- Registration', '--- Virtual visit'];
-  documentC: any = ['-- OpenKM', '-- Microsoft SharePoint'];
+  healthCategory: any = ['-- Patients portal', '-- Doctors portal'];
+  remoteCategory: any = ['--- Registration', '--- Virtual visit'];
+  documentCategory: any = ['-- OpenKM', '-- Microsoft SharePoint'];
 
   constructor(private formBuilder: FormBuilder) {
-    this.feedbackForm = this.formBuilder.group({
-      name: new FormControl(null),
-      email: new FormControl(null),
-      text: new FormControl(null),
-      category: new FormControl(null)
-    });
+    this.feedbackForm = this.buildForm();
   }
 
   // this.nameControl = this.feedbackForm.get('name') as FormControl;
   // this.nameControl.setValue('Mark');
+  // tslint:disable-next-line:typedef
+  // getHealthCategory(){
+  //   return this.feedbackForm.get('name');
+  // }
+  buildForm(): FormGroup {
+    return  this.formBuilder.group({
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+      text: new FormControl(null, Validators.required),
+      category: new FormControl(null, Validators.required)
+    });
+  }
+  submitFeedbackForm(): void {
+    // @ts-ignore
+    if (this.name().valid && this.email().valid && this.text().valid && this.category().valid){
+      this.feedbackForm.reset();
+    }
+    // console.log(this.feedbackForm.value);
+    // console.log(this.feedbackForm.get('email')?.value);
+    // this.feedbackForm.reset();
+  }
 
-  ngOnInit(): void {
+  name(): AbstractControl | null{
+    return this.feedbackForm.get('name');
+  }
+
+  email(): AbstractControl | null{
+    return this.feedbackForm.get('email');
+  }
+
+  text(): AbstractControl | null{
+    return this.feedbackForm.get('text');
+  }
+
+  category(): AbstractControl | null{
+    return this.feedbackForm.get('category');
+  }
+
+  checkNameValidation(): boolean{
+    // @ts-ignore
+    return this.name().invalid && this.name().touched;
+  }
+
+  checkEmailValidation(): boolean{
+    // @ts-ignore
+    return this.email().invalid && this.email().touched;
+  }
+
+  checkTextValidation(): boolean{
+    // @ts-ignore
+    return this.text().invalid && this.text().touched;
+  }
+
+  checkCategoryValidation(): boolean{
+    // @ts-ignore
+    return this.category().invalid && this.category().touched;
   }
 
 }
